b486888b936469fb57f704f02e50fd1934e1e7ff
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-09 16:04:54 +0600

    add html and javascript for the project

---
+++ index.js
@@ -0,0 +1,23 @@
+var date = new Date();
+document.write(date+"<br/>");
+
+var year = date.getFullYear();
+document.write(year+"<br/>");
+
+var month = date.getMonth();
+document.write(month+"<br/>");
+
+var day = date.getDay();
+document.write(day+"<br/>");
+
+var date = date.getDate();
+document.write(date+"<br/>");
+
+var date = date.getTime();
+document.write(date+"<br/>");
+
+var date = date.getSeconds()
+document.write(date+"<br/>");
+
+var date = date.getMilliseconds();
+document.write(date+"<br/>");
